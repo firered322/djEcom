88c7f33729f75cf22328421c67b37b4b2d4ab8fe
Atul <atulsingh1396@gmail.com>
2020-06-17 11:08:40 +0530

    added tags and defined the relationships between models'

--- accounts/models.py
+++ accounts/models.py
@@ -11,16 +11,25 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
         ('Outdoor', 'Outdoor')
     )
 
+    tags = models.ManyToManyField(Tag)
+
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -33,6 +42,11 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered')
     )
+
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
 
     status = models.CharField(max_length=200, null=True, choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True)
