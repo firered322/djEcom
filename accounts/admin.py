003e15032c1ff2c42844087853905c5579648d28
Atul <atulsingh1396@gmail.com>
2020-06-17 10:25:35 +0530

    added order and product model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer
+from .models import Customer, Order, Product
 
 admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Product)
