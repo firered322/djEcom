2efb01db17cf8e8592bfe4b4143b4c7b207c37f5
Atul <atulsingh1396@gmail.com>
2020-06-19 00:31:34 +0530

    added order update using form and instance variable

--- accounts/views.py
+++ accounts/views.py
@@ -40,3 +40,17 @@ def createOrder(request):
             return redirect('/')
     context = {'form': form}
     return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
