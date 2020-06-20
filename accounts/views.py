af2b3b8ddb89e96c86a9457779ee20deadeb32f3
Atul <atulsingh1396@gmail.com>
2020-06-20 07:57:24 +0530

    added login

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import *
 from .forms import OrderForm, CreateUserForm
 
@@ -21,7 +22,20 @@ def registerPage(request):
 
 def loginPage(request):
     context = {}
-    return render(request, 'accounts/login.html')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, "Username or password incorrect")
+            return render(request, 'accounts/login.html', context)
+
+    return render(request, 'accounts/login.html', context)
 
 
 def home(request):
